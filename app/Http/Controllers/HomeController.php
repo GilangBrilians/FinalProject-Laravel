a232ba3732d036e5b07ced55c8a372559e175331
GilangBrilians <gilangbrilians.f@gmail.com>
2024-02-15 22:29:00 +0700

    update home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,86 +3,13 @@
 namespace App\Http\Controllers;
   
 use Illuminate\Http\Request;
-use App\Models\Products; 
-use App\Models\Categories; 
-use App\Models\Users; 
-use App\Models\OrderItem; 
-use App\Models\Orders; 
 
 
 class HomeController extends Controller
 {
-
-    public function getTotalSubtotal()
-    {
-        return OrderItem::sum('subtotal');
-    }
-
-    public function getTotalCategoriesCount()
-    {
-        $totalCategoriesCount = Categories::count();
-        
-        return $totalCategoriesCount;
-    }
-    
-    public function getTotalUsersCount()
-    {
-        $totalUsersCount = Users::count();
-        
-        return $totalUsersCount;
-    }
-    
-    public function getTotalProductsCount()
-    {
-        $totalProductsCount = Products::count();
-        
-        return $totalProductsCount;
-    }
-
-    public function getTotalNewCount()
-    {
-        return Orders::where('order_status', 'new')->count();
-    }
-    
-    public function getTotalInProgressOrdersCount()
-    {
-        return Orders::where('order_status', 'on_progress')->count();
-    }
-
-    public function getTotalDoneCount()
-    {
-        return Orders::where('order_status', 'done')->count();
-    }
-
-    public function getTotalDeliveredCount()
-    {
-        return Orders::where('order_status', 'delivered')->count();
-    }
     
     public function index()
     {
-        $totalSubtotal = $this->getTotalSubtotal(); 
-
-        $totalProducts = $this->getTotalProductsCount();
-        $totalCategories = $this->getTotalCategoriesCount();
-        $totalUsers = $this->getTotalUsersCount();
-        $totalOnProgress = $this->getTotalInProgressOrdersCount();
-        $totalDone = $this->getTotalDoneCount();
-        $totalDelivered = $this->getTotalDeliveredCount();
-        $totalNew = $this->getTotalNewCount();
-
-        $totalSubtotalIDR = 'Rp ' . number_format($totalSubtotal, 0, ',', '.');
-
-        return view('Pages.home', [
-            'totalProducts' => $totalProducts,
-            'totalCategories' => $totalCategories,
-            'totalUsers' => $totalUsers,
-            'totalOnProgress' => $totalOnProgress,
-            'totalDone' => $totalDone,
-            'totalDelivered' => $totalDelivered,
-            'totalNew' => $totalNew,
-            'totalSubtotal' => $totalSubtotalIDR,
-
-        ]);
+        return view('Pages.home');
     }
 }
--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -31,7 +31,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                         Total Omset</div>                    
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalSubtotal }}</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -48,7 +48,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                         Customer</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalUsers }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -67,7 +67,7 @@
                                     </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalCategories }}</div>                                        
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>                                        
                                         </div>
                                     </div>
                                 </div>
@@ -86,7 +86,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                         Produk</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalProducts }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -105,7 +105,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                         Orderan Baru</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalNew }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -122,7 +122,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                         Orderan Sedang Diproses</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalOnProgress }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -141,7 +141,7 @@
                                     </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalDelivered }}</div>                                        
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>                                        
                                         </div>
                                     </div>
                                 </div>
@@ -160,7 +160,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                         Orderan Selesai </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalDone }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-comments fa-2x text-gray-300"></i>
